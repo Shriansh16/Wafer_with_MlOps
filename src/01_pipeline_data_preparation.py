fd33a369a82c4c508e593cdc370d9bef5b2e165a
Shriansh16 <shrianshsingh20@gmail.com>
2024-04-18 17:20:52 +0530

    first commit

---
+++ src/01_pipeline_data_preparation.py
@@ -0,0 +1,15 @@
+import os
+import argparse
+import yaml
+
+
+
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    default_config_path = os.path.join("config", "params.yaml")
+    args.add_argument("--config", default=default_config_path)
+    args.add_argument("--datasource", default=None)
+
+    parsed_args = args.parse_args()
+    main(config_path=parsed_args.config , datasource=parsed_args.datasource)
